c7f71f4156eefe0dec3b4f2bc1ca313675300e63
shimeeuisuk <euisuk95@gmail.com>
2024-01-31 21:07:11 +0900

    refactor: add my soulution Section8 6.js

--- Section8/6.js
+++ Section8/6.js
@@ -1,3 +1,5 @@
+// 바둑이 승차
+// 강의 풀이
 function solution(c, arr) {
   let answer = Number.MIN_SAFE_INTEGER;
   let n = arr.length;
@@ -13,3 +15,25 @@ function solution(c, arr) {
   DFS(0, 0);
   return answer;
 }
+
+// 내풀이
+const solution = (c, arr) => {
+  const n = arr.length;
+  const total = c;
+  let maxWeight = 0;
+  const DFS = (L, sum) => {
+    if (L === n) {
+      if (sum <= total && maxWeight < sum) {
+        maxWeight = sum;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  };
+
+  DFS(0, 0);
+  return maxWeight;
+};
+
+console.log(solution(259, [81, 58, 42, 33, 61]));
