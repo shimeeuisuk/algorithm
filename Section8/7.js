769ed762b729660ac9a456b771eb53af31160f81
shimeeuisuk <euisuk95@gmail.com>
2024-02-01 15:23:59 +0900

    feat: add Section8(DFS) 7.js

--- Section8/7.js
+++ Section8/7.js
@@ -1,2 +1,33 @@
-// 이진트리 푼다 못푼다
-// DFS (깊이, 점수합, 시간합)???
+// 최대점수 구하기(DFS)
+
+// 내 풀이
+const solution = (input) => {
+  const questionCount = input[0][0];
+  let limitTime = input[0][1];
+  const quesetionData = input.slice(1);
+  let maxScore = 0;
+
+  const DFS = (v, score, time) => {
+    if (time > limitTime) return;
+    if (v === questionCount) {
+      if (time <= limitTime && score > maxScore) maxScore = score;
+    } else {
+      DFS(v + 1, score + quesetionData[v][0], time + quesetionData[v][1]);
+      DFS(v + 1, score, time);
+    }
+  };
+
+  DFS(0, 0, 0);
+  return maxScore;
+};
+
+console.log(
+  solution([
+    [5, 20],
+    [10, 5],
+    [25, 12],
+    [15, 8],
+    [6, 3],
+    [7, 4],
+  ])
+);
