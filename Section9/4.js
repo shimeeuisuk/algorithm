e1674890b8c53b2caf3b9d0c0578eed675820beb
shimeeuisuk <euisuk95@gmail.com>
2024-02-02 16:24:10 +0900

    feat: BFS 이진 트리 넓이 우선 탐색 Section9 4.js

--- Section9/4.js
+++ Section9/4.js
@@ -1,36 +1,18 @@
-function s(board) {
-  let count = 0;
-  let dx = [-1, 0, 1, 0];
-  let dy = [0, 1, 0, -1];
-  function DFS(x, y) {
-    if (x === 6 && y === 6) count++;
-    else {
-      for (let k = 0; k < 4; k++) {
-        let nx = x + dx[k];
-        let ny = y + dy[k];
-        //행렬 밖에 나가면 안됨 //가려고 하는 지점이 0이어야함
-        if (nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6 && board[nx][ny] === 0) {
-          board[nx][ny] = 1;
-          DFS(nx, ny);
-          board[nx][ny] = 0;
-        }
-      }
+// BFS 이진트리 넓이 우선 탐색
+const solution = () => {
+  let answer = "";
+  let queue = [];
+  queue.push(1);
+
+  while (queue.length > 0) {
+    let v = queue.shift();
+    answer += v + " ";
+    for (let nv of [v * 2, v * 2 + 1]) {
+      if (nv > 7) continue;
+      queue.push(nv);
     }
   }
+  return answer;
+};
 
-  board[0][0] = 1;
-  DFS(0, 0);
-  return count;
-}
-
-let arr = [
-  [0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 1, 1, 1, 1, 0],
-  [0, 0, 0, 1, 0, 0, 0],
-  [1, 1, 0, 1, 0, 1, 1],
-  [1, 1, 0, 0, 0, 0, 1],
-  [1, 1, 0, 1, 1, 0, 0],
-  [1, 0, 0, 0, 0, 0, 0],
-];
-
-console.log(s(arr));
+console.log(solution());
