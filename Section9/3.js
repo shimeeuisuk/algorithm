7205b459003ea1f3812f8c22d8db9fef0da33d5b
shimeeuisuk <euisuk95@gmail.com>
2024-02-02 16:08:33 +0900

    feat: DFS 미로탐색 Section9 3.js

--- Section9/3.js
+++ Section9/3.js
@@ -1,41 +1,39 @@
-//n은 종착노드
+// DFS 미로탐색
 
-function s(n, arr) {
+const solution = (board) => {
   let answer = 0;
-  let graph = Array.from(Array(n + 1), () => Array());
-  let check = Array.from({ length: n + 1 }, () => 0);
-  //인접 리스트 만들기
-  for (let [a, b] of arr) {
-    graph[a].push(b);
-  }
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
 
-  function DFS(v) {
-    if (v === n) answer++;
+  const DFS = (x, y) => {
+    if (x === 6 && y === 6) answer++;
     else {
-      for (let i = 0; i < graph[v].length; i++) {
-        //이제 당연히 가니까 체크만 확인 하면 됨
-        if (check[graph[v][i]] === 0) {
-          check[graph[v][i]] = 1; //체크한다.
-          DFS(graph[v][i]); //i 노드로 간다.
-          check[graph[v][i]] = 0; // 다시 빽하는것
+      for (let k = 0; k < 4; k++) {
+        // 다음좌표
+        let nx = x + dx[k];
+        let ny = y + dy[k];
+        if (nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6 && board[nx][ny] === 0) {
+          board[nx][ny] = 1;
+          DFS(nx, ny);
+          board[nx][ny] = 0;
         }
       }
     }
-  }
-  check[1] = 1; // 첫번째 꼭 체크 실수 많이 함.
-  DFS(1);
-  return answer;
-}
+  };
 
-let arr = [
-  [1, 2],
-  [1, 3],
-  [1, 4],
-  [2, 1],
-  [2, 3],
-  [2, 5],
-  [3, 4],
-  [4, 2],
-  [4, 5],
-];
-console.log(s(5, arr));
+  board[0][0] = 1;
+  DFS(0, 0);
+  return answer;
+};
+
+console.log(
+  solution([
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [1, 1, 0, 1, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 1],
+    [1, 1, 0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0],
+  ])
+);
